1ba242070e9b33c1b8bcad53090db7591cf2136b
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-07-09 12:26:26 +0100

    Update UserController and registration form to validate incoming fields
    Using validate() method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,8 +6,15 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function register()
+    // Request and $request
+    public function register(Request $request)
     {
+        // validate() method
+        $incomingFields = $request->validate([
+            'username' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
         return 'Hello from register function';
     }
 }
