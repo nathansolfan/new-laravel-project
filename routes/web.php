ec07030b0323523a11f1cecd7ecdc88b326837fd
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-07-09 12:18:47 +0100

    Update users table migration to make username field unique and add UserController for user registration

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ExampleController;
 
 // ( '/', [a,b]) where a = class, b = actual page
 Route::get('/', [ExampleController::class, "homepage"]);
 
 Route::get('/about', [ExampleController::class, "aboutPage"]);
+
+// takes 2 args, the path and func/method
+Route::post('/register', [UserController::class, 'register']);
