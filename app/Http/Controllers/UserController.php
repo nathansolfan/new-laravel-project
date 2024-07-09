b466f6d6ff2f664fbe3557fd591d31bb385986e8
Nathangit config --global user.email nathansolfan@hotmail.com <78206940+nathansolfan@users.noreply.github.com>
2024-07-09 15:32:27 +0100

    Update UserController and User model to use 'username' instead of 'name' for registration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,9 +15,14 @@ class UserController extends Controller
         // validate() method
         $incomingFields = $request->validate([
             'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
-            'email' => 'required',
-            'password' => 'required',
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8', 'confirmed'],
         ]);
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+
+
+
         User::create($incomingFields);
         return 'Hello from register function';
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,6 +41,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            // automatically hash
             'password' => 'hashed',
         ];
     }
